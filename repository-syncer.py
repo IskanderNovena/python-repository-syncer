7d2f14a0a65d42047720514d2fd0199e2ef4f1b0
Sebastiaan Brozius <sebastiaan.brozius@solvinity.com>
2021-12-27 15:57:50 +0100

    Implemented subprocess for command execution

--- repository-syncer.py
+++ repository-syncer.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+import subprocess
 
 # Opening the file as part of loading the JSON-file automatically closes the
 # file after loading it. Otherwise, an explicit 'close' has to be done.
@@ -16,22 +17,31 @@ if not os.path.exists(settings['workingDirectory']):
 def sync_repo(source, destination):
     # Get the name of the repository from the source
     repo_dir = source.rsplit('/', 1)[-1]
+    full_repo_dir = os.path.join(settings['workingDirectory'], repo_dir)
     os.chdir(settings['workingDirectory'])
     try:
-        os.system('git clone --bare {}'.format(source))
-        os.chdir('./{}'.format(repo_dir))
-        os.system('git push --mirror {}'.format(destination))
-        os.chdir(settings['workingDirectory'])
-    except:
-        raise Exception('Something went horribly wrong!!')
+        subprocess.check_call(["git", "clone", "--bare", "{}".format(source)])
+        print(os.getcwd())
+        os.chdir(full_repo_dir)
+        print(os.getcwd())
+        subprocess.check_call(
+            ["git", "push", "--mirror", "{}".format(destination)])
+    except subprocess.CalledProcessError as error:
+        raise Exception("ERROR: Command '{}' failed with exit code '{}'".format(
+            ' '.join(error.cmd), error.returncode))
     finally:
-        if os.path.exists(os.path.join(settings['workingDirectory'], repo_dir)):
-            shutil.rmtree(repo_dir)  # , ignore_errors=True)
+        if os.path.exists(full_repo_dir):
+            print('Cleaning up directory...')
+            shutil.rmtree(full_repo_dir)
 
 
 for repo in repositories:
-    print('Processing sync for:\n  Source      : {}\n  Destination : {}\n'.format(
+    print('\nProcessing sync for:\n  Source      : {}\n  Destination : {}\n'.format(
         repo['source'], repo['destination']))
-    sync_repo(repo['source'], repo['destination'])
-      
-    print("\nDone!\nAll repositories have been processed.")
+    try:
+        sync_repo(repo['source'], repo['destination'])
+    except Exception as error:
+        print('\n{}'.format(error))
+        print('Processing has been interrupted!')
+    else:
+        print("\nDone!\nAll repositories have been processed.")
